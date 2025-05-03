4136be5e353cd54d44cd2c0ecde8020e6f5d592c
ZenoT3L <centients3@gmail.com>
2025-05-03 18:07:59 +0100

    Created a usestate for the notes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
-// import { useState } from "react";
+import { useState } from "react";
 import Header from "./components/Header";
 import Footer from "./components/footer";
 import Note from "./components/note";
 import CreateArea from "./components/CreateArea";
 
 function App() {
+  const [notes, setNotes] = useState([
+    {
+      title: "First Note",
+      content: "How does it work?",
+    },
+    {
+      title: "Second Note",
+      content: "It worked?",
+    },
+  ]);
+
   return (
     <>
       <Header />
       <CreateArea />
-      <Note key={1} title="Note title" content="Note content" />
+      {notes.map((note, index) => (
+        <Note key={index} title={note.title} content={note.content} />
+      ))}
       <Footer />
     </>
   );
